f4796b163ee85fb89d4513a0826554ea27a8f618
stebrand <sbr16@hotmail.es>
2023-05-08 13:48:48 -0500

    Segurity hardcoded on WEB

---
+++ Sales/Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var stevenUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Steven"),
+                new Claim("LastName", "Brand"),
+                new Claim(ClaimTypes.Name, "brand@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            },
+            authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(stevenUser)));
+        }
+    }
+
+}
